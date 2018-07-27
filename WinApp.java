b7692d0d6ab46dad8283848c60b745ac9ea5ffd0
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2018-07-27 10:01:04 +0900

    コメント追加, ウィンドウの名前を変更

--- WinApp.java
+++ WinApp.java
@@ -4,20 +4,23 @@ import java.awt.event.*;
 // MyFrameクラスの宣言
 class MyFrame extends Frame {
   public MyFrame() {
-    setSize(500, 800);
-    addWindowListener(new MyWindowAdapter());
+    setTitle("簡易計算機"); // ウィンドウのタイトル
+    setSize(500, 800); // ウィンドウサイズ
+    addWindowListener(new MyWindowAdapter()); // WindowEventを受け取るイベントリスナを指定する
   }
 }
 
+// イベントリスナー: MyWindowAdapter
+// 機能: ウィンドウがクローズ状態になったら終了する
 class MyWindowAdapter extends WindowAdapter {
   public void windowClosing(WindowEvent e) {
-    System.exit(0);
+    System.exit(0); // 終了処理
   }
 }
 
 public class WinApp {
   public static void main(String args[]) {
-    MyFrame mf = new MyFrame();
-    mf.setVisible(true);
+    MyFrame mf = new MyFrame(); // MyFrameオブジェクトを生成
+    mf.setVisible(true); // ウィンドウを表示できるようにする
   }
 }
