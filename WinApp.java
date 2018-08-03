9d1edac732eeaef3aabc24646bc857957ce24ce7
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2018-08-03 10:57:59 +0900

    ボタンが9こも表示されるよ

--- WinApp.java
+++ WinApp.java
@@ -4,32 +4,30 @@ import java.awt.event.*;
 // MyFrameクラスの宣言
 // ActionListenerインターフェースを実装
 class MyFrame extends Frame implements ActionListener {
-  Button b1; // ボタンオブジェクトを生成
   Label dispLabel; // 計算結果等を示すラベルを生成
+  Button btn[] = new Button[9];
+  String buttonLavel[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
   public MyFrame() {
     setTitle("簡易計算機"); // ウィンドウのタイトル
-    setSize(500, 800); // ウィンドウサイズ
+    setSize(1920, 1080); // ウィンドウサイズ
     addWindowListener(new MyWindowAdapter()); // WindowEventを受け取るイベントリスナを指定する
     setLayout(new FlowLayout()); // 左から右にボタンを配置する
     dispLabel = new Label("Hello");
-    b1 = new Button("1");
-    b1.addActionListener(this); // いベントリスナーを登録
-    add(b1);
+    for (int i = 0; i < 9; i++) {
+      btn[i] = new Button("way");
+      add(btn[i]);
+    }
     add(dispLabel);
   }
 
   // ボタンがクリックされたときのアクションを記述
   public void actionPerformed(ActionEvent ae) {
-    if (ae.getSource() == b1) {
-      System.out.println("クリックされたよ");
-      dispLabel.setText("Hello");
-    }
+    System.out.println("hey");
   }
 }
 
 // イベントリスナー: MyWindowAdapter
-// 機能: ウィンドウがクローズ状態になったら終了する
 class MyWindowAdapter extends WindowAdapter {
   public void windowClosing(WindowEvent e) {
     System.exit(0); // 終了処理
